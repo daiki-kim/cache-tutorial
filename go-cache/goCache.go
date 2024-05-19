1e5f0bc8636cb65408a19e95473ed098de0c02a5
Daiki Kim <kimdaiki5021@gmail.com>
2024-05-19 17:12:26 +0900

    Add comment for Zenn blog

--- go-cache/goCache.go
+++ go-cache/goCache.go
@@ -8,10 +8,13 @@ import (
 )
 
 func main() {
+	// キャッシュデータの有効期限が5分、10分ごとにキャッシュをリフレッシュする
 	c := cache.New(5*time.Minute, 10*time.Minute)
 
+	// キャッシュにデータを保存
 	c.Set("key", "value", cache.DefaultExpiration)
 
+	// キャッシュからデータを取得
 	if x, found := c.Get("key"); found {
 		fmt.Println("Found value:", x)
 	}
--- main.go
+++ main.go
@@ -29,10 +29,12 @@ func init() {
 	db.AutoMigrate(&Person{})
 	DB = db
 
+	// キャッシュの初期設定
 	Cache = cache.New(5*time.Minute, 10*time.Minute)
 }
 
 func GetPerson(ID int) (*Person, error) {
+	// キャッシュから指定のデータを取得
 	if x, found := Cache.Get(fmt.Sprintf("person_%d", ID)); found {
 		fmt.Println("got from cache")
 		return x.(*Person), nil
@@ -42,6 +44,7 @@ func GetPerson(ID int) (*Person, error) {
 	if err := DB.First(&person, ID).Error; err != nil {
 		return nil, err
 	}
+	// データベースにアクセスして得たデータをキャッシュに保存
 	Cache.Set(fmt.Sprintf("person_%d", ID), &person, cache.DefaultExpiration)
 	fmt.Println("got from db")
 	return &person, nil
@@ -51,10 +54,12 @@ func main() {
 	person := Person{Name: "Jinzhu", Age: 18}
 	DB.Create(&person)
 
+	// 1回目（データベースから取得）
 	gotPerson, _ := GetPerson(person.ID)
 	fmt.Println(gotPerson.Name)
 	fmt.Println(gotPerson.Age)
 
+	// 2回目（キャッシュから取得）
 	gotPerson, _ = GetPerson(person.ID)
 	fmt.Println(gotPerson.Name)
 	fmt.Println(gotPerson.Age)
